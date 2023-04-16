377f17abdc7d161d4c74b341cb98c3c5b0486393
Joaquin Leklere <jleklere@yahoo.com.ar>
2023-04-16 13:59:27 -0300

    Added projects to My Work section

--- src/Components/SectionWork.jsx
+++ src/Components/SectionWork.jsx
@@ -1,5 +1,10 @@
 import styles from "./SectionWork.module.css";
 import Adeip from "../Assets/ADEIP.png";
+import Barberstudio96 from "../Assets/Barberstudio96.png";
+import Sportswear from "../Assets/Sportswear.png";
+import Portfolio from "../Assets/Portfolio.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquareArrowUpRight } from "@fortawesome/free-solid-svg-icons";
 
 function SectionWork() {
   return (
@@ -7,18 +12,54 @@ function SectionWork() {
       <h3 className={styles["work"]}>My Work</h3>
       <div className={styles["projects"]}>
         <div className={styles["project"]}>
-          <a className={styles["project-adeip--link"]} href="https://adeip.net">
-            <img src={Adeip} className={styles["project-adeip--img"]} alt="" />
+          <a className={styles["project-link"]} href="https://adeip.net">
+            <img src={Adeip} className={styles["project-img"]} alt="" />
+            <FontAwesomeIcon
+              className={styles["project-link--icon"]}
+              icon={faSquareArrowUpRight}
+            />
           </a>
           <h4>ADEIP</h4>
+          <p className={styles["project-description"]}></p>
         </div>
         <div className={styles["project"]}>
-          <h4>BARBER STUDIO 96'</h4>
+          <a
+            className={styles["project-link"]}
+            href="https://barberstudio96.netlify.com"
+          >
+            <img
+              src={Barberstudio96}
+              className={styles["project-img"]}
+              alt=""
+            />
+            <FontAwesomeIcon
+              className={styles["project-link--icon"]}
+              icon={faSquareArrowUpRight}
+            />
+          </a>
+          <h4>Barber Studio 96'</h4>
         </div>
         <div className={styles["project"]}>
+          <a className={styles["project-link"]} href="/">
+            <img src={Sportswear} className={styles["project-img"]} alt="" />
+            <FontAwesomeIcon
+              className={styles["project-link--icon"]}
+              icon={faSquareArrowUpRight}
+            />
+          </a>
           <h4>SportsWear</h4>
         </div>
         <div className={styles["project"]}>
+          <a
+            className={styles["project-link"]}
+            href="https://j-leklere.netlify.com"
+          >
+            <img src={Portfolio} className={styles["project-img"]} alt="" />
+            <FontAwesomeIcon
+              className={styles["project-link--icon"]}
+              icon={faSquareArrowUpRight}
+            />
+          </a>
           <h4>My Website</h4>
         </div>
       </div>
